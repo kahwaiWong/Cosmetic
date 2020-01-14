4880367db3d2ba93bc34cbfd328687e5bfdf9998
kahwaiWong <kahwai.blue@gmail.com>
2020-01-14 13:45:00 +0800

    initial commit

--- app/src/main/java/com/example/cosmetic/MainActivity.kt
+++ app/src/main/java/com/example/cosmetic/MainActivity.kt
@@ -1,17 +1,39 @@
 package com.example.cosmetic
 
+import android.content.ContentValues.TAG
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.ImageView
 import android.widget.Toast
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
-class MainActivity : AppCompatActivity(), View.OnClickListener {
+class MainActivity : AppCompatActivity(), myRef.addValueEventListener(object: ValueEventListener {
+
+    override fun onDataChange(dataSnapshot: DataSnapshot) {
+        // This method is called once with the initial value and again
+        // whenever data at this location is updated.
+        val value = dataSnapshot.getValue(String::class.java)
+        Log.d(TAG, "Value is: $value")
+    }
+
+    override fun onCancelled(error: DatabaseError) {
+        // Failed to read value
+        Log.w(TAG, "Failed to read value.", error.toException())
+    }
+}), View.OnClickListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val database = FirebaseDatabase.getInstance()
+        val myRef = database.getReference("cosmetic")
+
 
         val button1 = findViewById<ImageView>(R.id.imageViewLip)
         val button2 = findViewById<ImageView>(R.id.imageViewEyes)
@@ -46,3 +68,4 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     }
 
 }
+
